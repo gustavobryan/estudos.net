5bfc80bc2a4465682bbc19accb1c2bfa108e276c
Gustavo Bryan <devgustavob@gmail.com>
2023-03-15 13:11:08 -0300

    inicio

---
+++ Helloworld/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Helloworld
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            // Int - número inteiros -1 10
+            // Float - números quebrados 1.4 ==> Double| Decimal
+            // Bool - true or false
+            // String - "texto"
+            // Char - ''
+
+
+            // A-Z, 0 - 9, SEM CARACTERES, SÓ _
+            // 
+            /*int segundaGuerraMundial = 1942;
+            string corFavorita = "azul";
+            float velocidadeF1 = 294.48f;
+            bool segGuerraMundial = true;
+
+            Console.WriteLine(segundaGuerraMundial);
+            Console.WriteLine(corFavorita);
+            Console.WriteLine(velocidadeF1);
+            Console.WriteLine(segGuerraMundial);
+
+            velocidadeF1 = 348.29f;
+
+            Console.WriteLine(velocidadeF1);
+            
+             var cor = "azul";*/
+            const float PI = 3.1455876879f;
+
+            Console.WriteLine(PI);
+
+            Console.ReadLine();
+
+
+        }
+    }
+}
